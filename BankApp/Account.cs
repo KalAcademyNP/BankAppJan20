6d2a0574182367f6cfaaf50ec8b885f307945b73
Kal <info@kalacademy.org>
2020-01-12 14:16:34 -0800

    Initial commit

---
+++ BankApp/Account.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BankApp
+{
+    /// <summary>
+    /// This represents a bank account
+    /// where you can withdraw or deposit
+    /// money into the account
+    /// </summary>
+    class Account
+    {
+        #region Properties
+        /// <summary>
+        /// Unique account number
+        /// </summary>
+        public int AccountNumber { get; set; }
+        public string AccountName { get; set; }
+        public string AccountType { get; set; }
+        public decimal Balance { get; set; }
+
+        public string EmailAddress { get; set; }
+        public DateTime CreatedDate { get; set; }
+        #endregion
+    }
+}
---
+++ BankApp/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace BankApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           var myAccount  = new Account();
+            myAccount.AccountNumber = 123456;
+            myAccount.AccountName = "My checking";
+
+            var myAccount2 = new Account();
+            myAccount2.AccountNumber = 34567;
+
+        }
+    }
+}
